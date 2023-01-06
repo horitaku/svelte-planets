831cfa7fe062e021dd9aa1b9e63241f6c1f58aeb
Takuya HORIKAWA <horikawa_takuya@benic.co.jp>
2023-01-06 17:57:37 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name
+    name = "takuya"
+</script>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
+<h1>Welcome to SvelteKit</h1>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+
+<p>hello {name}</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+npm
+
+<button class="btn">Hello daisyUI</button>
